6823069aedb154e6586d307c7c5d57a501cef995
o00110001o <45133321+o00110001o@users.noreply.github.com>
2023-01-19 19:49:46 -0300

    Add files via upload

--- app/urls.py
+++ app/urls.py
@@ -3,4 +3,19 @@ from .views import *
 
 urlpatterns = [
     path('', index, name="index"),
+    path('api', api, name="api"),
+    path('carrito', carrito, name="carrito"),
+    path('contacto', contacto, name="contacto"),
+    path('estadopedido', estadopedido, name="estadopedido"),
+    path('estadopedidoadmin', estadopedidoadmin, name="estadopedidoadmin"),
+    path('estadopedidousuario', estadopedidousuario, name="estadopedidousuario"),
+    path('formulario', formulario, name="formulario"),
+    path('formularioSuscripcion', formularioSuscripcion, name="formularioSuscripcion"),
+    path('index_formualrioSuscripcion', index_formualrioSuscripcion, name="index_formualrioSuscripcion"),
+    path('/', , name=""),
+    path('/', , name=""),
+    path('/', , name=""),
+    path('/', , name=""),
+    path('/', , name=""),
+    path('/', , name=""),
 ]
--- app/views.py
+++ app/views.py
@@ -3,3 +3,50 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request,'index.html')
+
+def api(request):
+    return render(request,'api.html')
+
+def carrito(request):
+    return render(request,'carrito.html')
+
+def contacto(request):
+    return render(request,'contacto.html')
+
+def estadopedido(request):
+    return render(request,'estadopedido.html')
+
+def estadopedidoadmin(request):
+    return render(request,'estadopedidoadmin.html')
+
+def estadopedidousuario(request):
+    return render(request,'estadopedidousuario.html')
+
+def formulario(request):
+    return render(request,'formulario.html')
+
+def formularioSuscripcion(request):
+    return render(request,'formularioSuscripcion.html')
+
+def index_formualrioSuscripcion(request):
+    return render(request,'index_formualrioSuscripcion.html')
+
+def index_user(request):
+    return render(request,'index_user.html')
+
+def login(request):
+    return render(request,'login.html')
+
+def productos(request):
+    return render(request,'productos.html')
+
+def registration(request):
+    return render(request,'registration.html')
+
+def suscripcion(request):
+    return render(request,'suscripcion.html')
+
+def terminos_condiciones(request):
+    return render(request,'terminos_condiciones.html')
+
+#CRUD PLANTAS
