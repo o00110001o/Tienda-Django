9bea88cd6f9d607b37b2b15827eaf099c2802f02
o00110001o <45133321+o00110001o@users.noreply.github.com>
2023-01-23 19:54:02 -0300

    Delete app directory

--- app/forms.py
+++
@@ -1,9 +0,0 @@
-from socket import fromshare
-from django import forms
-from .models import Planta
-
-class PlantaForm(forms.ModelForm):
-    class Meta:
-        model = Planta
-        fields = '__all__'
-    
